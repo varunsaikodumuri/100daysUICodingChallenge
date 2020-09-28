f75503938de1a9040c275144267a512c414e3c4d
Varunsai Kodumuri <varunsai.kodumuri@gmail.com>
2020-09-29 00:37:13 +0530

    Day 20 - Pulse Animation

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -9,7 +9,7 @@ export class DataService {
   constructor() {
     this.navList = [
       { name: 'Day 1 + 2 (Image Gallery)', path: '/day1', showHeader: true },
-      { name: 'Day 3 + 4 (Minesweeper)', path: '/day3',  showHeader: true },
+      { name: 'Day 3 + 4 (Minesweeper)', path: '/day3', showHeader: true },
       { name: 'Day 5 + 6 (Image Slider)', path: '/day5', showHeader: true },
       { name: 'Day 7 (Image Carousel)', path: '/day7', showHeader: true },
       { name: 'Day 8 (Flex Box)', path: '/day8', showHeader: true },
