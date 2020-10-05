b20db949daa046cf996c285c93da8879ddd76e91
Varunsai Kodumuri <varunsai.kodumuri@gmail.com>
2020-10-06 00:13:39 +0530

    Day 22 - Intersection Observer

--- src/app/landing-page-v1/landing-page-v1.component.ts
+++ src/app/landing-page-v1/landing-page-v1.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page-v1',
   templateUrl: './landing-page-v1.component.html',
   styleUrls: ['./landing-page-v1.component.scss']
 })
-export class LandingPageV1Component implements OnInit {
-
-  constructor() { }
+export class LandingPageV1Component implements OnInit, AfterViewInit {
+  constructor(private elem: ElementRef) { }
 
   ngOnInit() {
   }
+
+  ngAfterViewInit(){
+    const faders = this.elem.nativeElement.querySelectorAll('.fade-in');
+    const products = this.elem.nativeElement.querySelectorAll('.product');
+    const appearOptions = {
+      rootMargin: "0px 0px -300px 0px"
+    };
+    const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+      entries.forEach(entry => {
+        if(!entry.isIntersecting) {
+          return;
+        } else {
+          entry.target.classList.add('appear');
+          appearOnScroll.unobserve(entry.target);
+        }
+
+      })
+    }, appearOptions);
+
+    faders.forEach(fader => {
+      appearOnScroll.observe(fader);
+    });
+
+    products.forEach(product => {
+      appearOnScroll.observe(product);
+    });
+}
 
 }
